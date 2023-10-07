508db3c17573bd14223c546e2799e4a52e3f1c5f
Juan Marmolejo <marmolejor@hotmail.com>
2023-10-07 00:49:50 +0000

    #10 first commit just to save changes

--- main.tf
+++ main.tf
@@ -4,7 +4,15 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.20.0"
+    }
   }
+}
+
+provider "aws" {
+  # Configuration options
 }
 
 provider "random" {
@@ -12,10 +20,21 @@ provider "random" {
 }
 
 resource "random_string" "bucket_name" {
+  lower = true
+  upper = false
   length           = 16
   special          = false
 }
 
+#https://registry.terraform.io/providers/hashicorp/random/latest/docs
 output "random_bucket_name" {
  value = random_string.bucket_name.result
+}
+
+
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+resource "aws_s3_bucket" "example" {
+  # Bucket Naming Rules
+  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html 
+  bucket = random_string.bucket_name.result
 }
