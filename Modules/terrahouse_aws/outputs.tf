303cf5f5875430b3941f0e7be392fcef612c9f24
Juan Marmolejo <marmolejor@hotmail.com>
2023-10-10 00:29:16 +0000

    #288 create cdn and serve s3 bucket from cdn

--- Modules/terrahouse_aws/outputs.tf
+++ Modules/terrahouse_aws/outputs.tf
@@ -5,3 +5,8 @@ output "bucket_name" {
 output "website_endpoint" {
   value = aws_s3_bucket_website_configuration.static.website_endpoint
 }
+
+#This should not be here I put it to see what happens
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
