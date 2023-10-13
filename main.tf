badf0ec241aa78fcde3c6d028517c48a7368d0ba
Juan Marmolejo <marmolejor@hotmail.com>
2023-10-13 23:44:03 +0000

    #38 implement terraform provider block in terraform

--- main.tf
+++ main.tf
@@ -1,9 +1,50 @@
-module "terrahouse_aws" {
-  source      = "./Modules/terrahouse_aws"
-  user_uuid   = var.user_uuid
-  bucket_name = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  content_version = var.content_version
-  assets_path = var.assets_path
+terraform {
+  required_providers {
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
+  }
+  #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "ExamPro"
+
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
+  #cloud {
+  #  organization = "ExamPro"
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
+
 }
+
+provider "terratowns" {
+  endpoint = "http://localhost:4567/api"
+  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
+  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+}
+
+# module "terrahouse_aws" {
+#   source      = "./Modules/terrahouse_aws"
+#   user_uuid   = var.user_uuid
+#   bucket_name = var.bucket_name
+#   index_html_filepath = var.index_html_filepath
+#   error_html_filepath = var.error_html_filepath
+#   content_version = var.content_version
+#   assets_path = var.assets_path
+# }
+
+
+# resource "terratowns_home" "home" {
+#   name = "Why Mario Bros is the best video game to start in video games"
+#   description = <<DESCRIPTION
+# DESCRIPTION
+#   #domain_name = module.terrahouse_aws.cloudfront_url
+#   domain_name = "3fdq3gz.cloudfront.net"
+#   town = "gamers-grotto"
+#   content_version = 1
+# }
--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
-output "bucket_name" {
-  description = "Bucket name for our static website hosting"
-  value       = module.terrahouse_aws.bucket_name
-}
+# output "bucket_name" {
+#   description = "Bucket name for our static website hosting"
+#   value       = module.terrahouse_aws.bucket_name
+# }
 
-output "s3_website_endpoint" {
-  description = "S3 static website hosting endpoint"
-  value       = module.terrahouse_aws.website_endpoint
-}
+# output "s3_website_endpoint" {
+#   description = "S3 static website hosting endpoint"
+#   value       = module.terrahouse_aws.website_endpoint
+# }
 
-#This should not be here I put it to see what happens
-output "account_id" {
-  value = module.terrahouse_aws.account_id
-}
+# #This should not be here I put it to see what happens
+# output "account_id" {
+#   value = module.terrahouse_aws.account_id
+# }
 
-output "cloudfront_url"{
- description = "The Cloudfront Distribution Domain Name"
- value = module.terrahouse_aws.cloudfront_url
-}
+# output "cloudfront_url"{
+#  description = "The Cloudfront Distribution Domain Name"
+#  value = module.terrahouse_aws.cloudfront_url
+# }
