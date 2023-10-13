d870227106161e9e15fcad905b4174b94f067c7c
Juan Marmolejo <marmolejor@hotmail.com>
2023-10-13 10:24:08 +0000

    #36 make sure that all our endpoints work correctly

--- terratowns_mock_server/server.rb
+++ terratowns_mock_server/server.rb
@@ -208,7 +208,6 @@ class TerraTownsMockServer < Sinatra::Base
     # Validate payload data
     name = payload["name"]
     description = payload["description"]
-    domain_name = payload["domain_name"]
     content_version = payload["content_version"]
 
     unless params[:uuid] == $home[:uuid]
@@ -217,10 +216,11 @@ class TerraTownsMockServer < Sinatra::Base
 
     home = Home.new
     home.town = $home[:town]
+    home.domain_name = $home[:domain_name]
     home.name = name
     home.description = description
-    home.domain_name = domain_name
     home.content_version = content_version
+
 
     unless home.valid?
       error 422, home.errors.messages.to_json
@@ -241,8 +241,9 @@ class TerraTownsMockServer < Sinatra::Base
     end
 
     # delete from mock database
+    uuid = $home[:uuid]
     $home = {}
-    { message: "House deleted successfully" }.to_json
+    { uuid: uuid }.to_json
   end
 end
 
