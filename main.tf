2176ee6af3478ad65b80bc013e82e4108fbe30ba
marmolejor <56137995+marmolejor@users.noreply.github.com>
2023-10-13 23:14:41 -0600

    #52 test our terrahome on terratowns missingo test town (#44)

--- main.tf
+++ main.tf
@@ -23,30 +23,29 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = "https://terratowns.cloud/api"
+  user_uuid=var.teacherseat_user_uuid
+  token=var.terratowns_access_token
 }
 
-# module "terrahouse_aws" {
-#   source      = "./Modules/terrahouse_aws"
-#   user_uuid   = var.user_uuid
-#   bucket_name = var.bucket_name
-#   index_html_filepath = var.index_html_filepath
-#   error_html_filepath = var.error_html_filepath
-#   content_version = var.content_version
-#   assets_path = var.assets_path
-# }
+module "terrahouse_aws" {
+  source      = "./Modules/terrahouse_aws"
+  user_uuid   = var.teacherseat_user_uuid
+  bucket_name = var.bucket_name
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  content_version = var.content_version
+  assets_path = var.assets_path
+}
 
 
-resource "terratowns_home" "home" {
+resource "terratowns_home" "mariohome" {
   name = "Why Mario Bros is the best video game to get started in video games"
   description = <<DESCRIPTION
   The "Super Mario Bros." video games series, starting with its initial release in 1985 for the Nintendo Entertainment System (NES), 
   is frequently heralded as a great entry point for those new to video games. 
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
-  town = "gamers-grotto"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  town = "missingo"
   content_version = 1
 }
