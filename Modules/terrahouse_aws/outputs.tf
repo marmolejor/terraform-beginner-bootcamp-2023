2cb9faf81b9d67550a2fb47cc8bbc76a38004c57
Juan Marmolejo <marmolejor@hotmail.com>
2023-10-14 00:32:26 +0000

    #38 adjusted outputs file

--- Modules/terrahouse_aws/outputs.tf
+++ Modules/terrahouse_aws/outputs.tf
@@ -6,11 +6,6 @@ output "website_endpoint" {
   value = aws_s3_bucket_website_configuration.static.website_endpoint
 }
 
-#This should not be here I put it to see what happens
-output "account_id" {
-  value = data.aws_caller_identity.current.account_id
-}
-
 output "cloudfront_url"{
  value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
